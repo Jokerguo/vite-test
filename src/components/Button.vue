073a1e9add408f64d3b4039ae4c4e0657c0b7cde
郭东生 <1977036313@qq.com>
2021-12-02 18:06:30 +0800

    vue3 属性绑定

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-    button 文档
-  </div>
+<p>Button 实例</p>
+<h1>实例1</h1>
+<Button @click="onClick"
+        @focus="onClick"
+        @mouseover="onClick"
+        size="small"
+>你好啊</Button>
+
 </template>
+
+<script setup>
+import Button from '../libs/Button.vue';
+
+const onClick = ()=>{
+  console.log(11111);
+}
+
+</script>
---
+++ src/libs/Button.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :size="size">
+    <button v-bind="rest">
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default{
+inheritAttrs : false,
+}
+</script>
+<script setup>
+import { useAttrs } from 'vue'
+const {size,...rest} = useAttrs() //剩余参数rest
+</script>
+
+
+
+<style scoped>
+div{
+  border: 1px solid red;
+}
+</style>
