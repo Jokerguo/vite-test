6d64e6b55448c226767fb809de3abbd5387f8913
郭东生 <1977036313@qq.com>
2021-12-02 16:12:33 +0800

    switch 添加props

---
+++ src/libs/Switch.vue
@@ -0,0 +1,45 @@
+<template>
+<button @click="toggle" :class="{checked:value}"><span></span></button>
+</template>
+
+<script setup>
+const props = defineProps({
+  value: {
+    type: Boolean
+  }
+})
+const emits = defineEmits(['change'])
+
+const toggle = ()=>{
+  emits('change',!props.value)
+}
+</script>
+
+<style lang="scss" scoped>
+  $h: 22px;
+  $h2: $h - 4px;
+  button{
+    height: $h;
+    width: $h*2;
+    border: none;
+    background: gray;
+    border-radius: $h/2;
+    position: relative;
+  }
+  span{
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background:white;
+    border-radius: $h2 / 2;
+    transition: all 250ms;
+  } 
+  button.checked{
+    background: blue;
+  }
+  button.checked > span{
+    left: calc(100% - $h2 - 2px);
+  }
+</style> 
