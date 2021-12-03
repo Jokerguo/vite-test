284c773871700eb870e741c615f2c22dc6c541de
郭东生 <1977036313@qq.com>
2021-12-03 17:11:25 +0800

    button组件完成

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,10 +1,20 @@
 <template>
 <p>Button 实例</p>
 <h1>实例1</h1>
-<Button @click="onClick"
-        @focus="onClick"
-        @mouseover="onClick"
->你好啊</Button>
+<Button>按钮</Button>
+<Button theme="button">按钮</Button>
+<Button theme="link">按钮</Button>
+<Button theme="text">按钮</Button>
+<h1>实例2</h1>
+<Button size="normal">正常</Button>
+<Button size="big">大大大大</Button>
+<Button size="small">小小</Button>
+<h1>实例3 disabled</h1>
+<Button disabled  @click="xxx">asd</Button>
+<Button :disabled="true">萨达</Button>
+<h1>实例3 loading</h1>
+<Button loading>loading中</Button>
+<Button>完成</Button>
 
 </template>
 
@@ -12,6 +22,6 @@
 import Button from '../libs/Button.vue';
 console.log(1111)
 
-const onClick = ()=>{ console.log(22221) }
+const xxx = ()=>{console.log(1111)}
 
 </script>
--- src/libs/Button.vue
+++ src/libs/Button.vue
@@ -1,21 +1,87 @@
 <template>
-  <div :size="size">
-    <button v-bind="$attrs">
-      <slot></slot>
-    </button>
-  </div>
+  <button class="gulu-button" :class="classes" :disabled="disabled">
+    <span v-if="loading" class="loading"></span>
+    <slot class="slot"></slot>
+  </button>
 </template>
 
-<script>
-export default{
-inheritAttrs : false,
-}
-</script>
 <script setup>
+const props = defineProps({
+  theme: {
+    type: String,
+    default: 'button',
+  },
+  size: {
+    type: String,
+    default: 'normal',
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  loading: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const classes = computed(() => {
+  return {
+    [`theme-${props.theme}`]: props.theme,
+    [`size-${props.size}`]: props.size,
+  }
+})
 </script>
 
-<style scoped>
-div{
+<style lang="scss">
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #40a9ff;
+$radius: 4px;
+.gulu-button {
+  box-sizing: border-box;
+  height: $h;
+  width: auto;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
+  & + & {
+    margin-left: 8px;
+  }
+  &:hover,
+  &:focus {
+    color: $blue;
+    border-color: $blue;
+  }
+  &:focus {
+    outline: none;
+  }
+  &::-moz-focus-inner {
+    border: 0;
+  }
+}
+.loading {
+  display: inline-block;
   border: 1px solid red;
+  border-bottom: 1px solid transparent;
+  animation: rotate 1s infinite linear;
+}
+
+@keyframes rotate {
+  0% {
+    transform: rotate(0);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
