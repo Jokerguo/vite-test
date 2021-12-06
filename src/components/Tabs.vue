81f95a1350ae1a1f4225bb1cef1002af49daaebc
郭东生 <1977036313@qq.com>
2021-12-06 13:57:07 +0800

    切换tab

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -3,12 +3,12 @@
 <h1>实例1</h1>
 <Tabs v-model:selected="selected">
   <Tab title="导航1">内容1</Tab>
-  <Tab title="导航2">内容2</Tab>
+  <Tab title="导航222">内容2</Tab>
 </Tabs>
 </template>
 
 <script setup>
   import Tabs from '../libs/Tabs.vue';
   import Tab from '../libs/Tab.vue';
-  const selected = ref('导航1')
+  const selected = ref('导航222')
 </script>
--- src/libs/Tabs.vue
+++ src/libs/Tabs.vue
@@ -1,47 +1,69 @@
 <template>
-<div class="gulu-tabs">
-  <div class="gulu-tabs-nav">
-    <div class="gulu-tabs-nav-item"
-    :class="{selected:selected === t}"
-    v-for="(t,index) in titles" 
-    :key="index"
-    @click="select(t)"
-    >{{t}}</div>
-  </div>
-  <div class="gulu-tabs-content">
-    <!-- <component class="gulu-tabs-content-item" 
+  <div class="gulu-tabs">
+    <div class="gulu-tabs-nav" ref="container">
+      <div
+        class="gulu-tabs-nav-item"
+        :class="{ selected: selected === t }"
+        v-for="(t, index) in titles"
+        :ref="setItemRef"
+        :key="index"
+        @click="select(t)"
+      >
+        {{ t }}
+      </div>
+      <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
+    </div>
+    <div class="gulu-tabs-content">
+      <!-- <component class="gulu-tabs-content-item" 
     v-for="(c,index) in defaults"
     :is="c" :key="index"
     /> -->
-    <!-- 需添加key -->
-      <component class="gulu-tabs-content-item" :is="current" :key="selected"/>
+      <!-- 需添加key -->
+      <component class="gulu-tabs-content-item" :is="current" :key="selected" />
+    </div>
   </div>
-</div>
 </template>
 
-<script setup>
-import Tab from './Tab.vue';
-import { useSlots } from 'vue';
+<script setup lang="ts">
+import Tab from './Tab.vue'
+import { useSlots } from 'vue'
+const refs = ref<HTMLDivElement[]>([])
+const indicator = ref<HTMLDivElement>(null)
+const container = ref<HTMLDivElement>(null)
 const slots = useSlots()
 const defaults = slots.default()
-const titles = defaults.map(tag=>tag.props.title)
-defaults.forEach(ele=>{
-  if(ele.type !== Tab){
+const titles = defaults.map((tag) => tag.props.title)
+defaults.forEach((ele) => {
+  if (ele.type !== Tab) {
     throw new Error('Tabs中只能使用Tab标签')
   }
 })
 const props = defineProps({
-  selected:{
-    type: String
+  selected: {
+    type: String,
   },
 })
 const emits = defineEmits(['update:selected'])
-const select = (t)=>{
-  emits('update:selected',t)
+const select = (t) => {
+  emits('update:selected', t)
 }
-const current = computed(()=>{
-  return defaults.filter(ele=>ele.props.title === props.selected)[0]
+const current = computed(() => {
+  return defaults.filter((ele) => ele.props.title === props.selected)[0]
 })
+const setItemRef = (el)=>{
+  if(el)refs.value.push(el)
+}
+const x = ()=>{
+  const divs = refs.value
+  const result = divs.filter(div=>div.classList.contains('selected'))[0]
+  const {width} = result.getBoundingClientRect()
+  indicator.value.style.width = width + 'px'
+  const {left:left1} = container.value.getBoundingClientRect()
+  const {left:left2} = result.getBoundingClientRect()
+  indicator.value.style.left = left2 - left1 + 'px' 
+}
+onMounted(x)
+onUpdated(x)
 </script>
 
 <style lang="scss">
@@ -53,6 +75,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
     &-item {
       padding: 8px 0;
       margin: 0 16px;
@@ -63,6 +86,15 @@ $border-color: #d9d9d9;
       &.selected {
         color: $blue;
       }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
     }
   }
   &-content {
