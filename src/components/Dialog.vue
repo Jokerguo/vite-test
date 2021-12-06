d8f6c16093844f40492eba664d1237d64c69a56a
郭东生 <1977036313@qq.com>
2021-12-06 09:57:53 +0800

    初始化dialog 接收visible

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    dialog 文档
-  </div>
+  <h1>实例1</h1>
+  <Button @click="toggle">按钮</Button>
+  <Dialog :visible="visible"/>
 </template>
+
+<script setup>
+  import Dialog from '../libs/Dialog.vue';
+  import Button from '../libs/Button.vue';
+  const visible = ref(false)
+  const toggle = ()=>{
+    visible.value = !visible.value
+  }
+</script>
